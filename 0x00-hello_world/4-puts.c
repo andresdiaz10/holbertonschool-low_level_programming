5e930a4d5b72ac40abc41925ab666547aa3772fb
andresdiaz10 <andresdiaz5010@icloud.com>
2021-06-21 00:46:48 +0000

    Task -4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * Description:	Program that prints a message to the standard output using the puts function.
+ */
+int main(void)
+{ 
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0); 
+} 
