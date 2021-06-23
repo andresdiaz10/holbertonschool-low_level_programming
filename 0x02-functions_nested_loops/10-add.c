729c53cdbdc5505d22ca7dd44cf32932a2f109c5
andresdiaz10 <andresdiaz5010@icloud.com>
2021-06-23 04:45:15 +0000

    Task 10 -11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.c"
+/**
+ * add - asd
+ * @a: assd
+ * @b: asdddf
+ *
+ * Return: asddd
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_to_98 - asdd
+ * @n: - asdds
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (n ; n > 97 ; n--)
+		{
+			printf("%d, ", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+		}
+	}
+	else
+	{
+		for (n ; n < 99 ; n++)
+		{
+			printf("%d, ", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+			}
+		}
+	}
+}
