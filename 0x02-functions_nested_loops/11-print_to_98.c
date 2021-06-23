e90c37ef14f00b4b3091c37bb7c75e5892bb57ad
andresdiaz10 <andresdiaz5010@icloud.com>
2021-06-23 05:11:12 +0000

    Fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (aux = n ; aux > 98 ; aux--)
+		for (aux = n ; aux >= 99 ; aux--)
 		{
 			printf("%d, ", aux);
 		}
@@ -21,7 +21,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (aux = n ; aux < 98 ; aux++)
+		for (aux = n ; aux <= 97 ; aux++)
 		{
 			printf("%d, ", aux);
 		}
