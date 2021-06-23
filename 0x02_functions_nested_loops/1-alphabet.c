ccaf617120c114701fa4a88599b91c347d8a65d7
andresdiaz10 <andresdiaz5010@icloud.com>
2021-06-23 00:06:30 +0000

    Task 0 - 1

---
+++ 0x02_functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Print the alphabet in lowercase using _putchar
+ */
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	putchar('\n');
+}
