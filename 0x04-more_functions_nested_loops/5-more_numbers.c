3f2a2161a5f3e4edd68bb0f3deb58bd8b7435c67
andresdiaz10 <andresdiaz5010@icloud.com>
2021-06-25 02:42:12 +0000

    Task -5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * void more_numbers - Print 10 times
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int number;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (number = 0 ; number < 15 ; number++)
+		{
+			if (number > 9)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i / 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
