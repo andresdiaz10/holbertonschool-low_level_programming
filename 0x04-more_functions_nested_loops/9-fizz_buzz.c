053c2f4b34becd4bb5c4b9f1ffc9ecd610ec5eac
andresdiaz10 <andresdiaz5010@icloud.com>
2021-06-25 05:05:36 +0000

    Fizz buzz task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Print fizz buzz xd
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i < 101 ; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
