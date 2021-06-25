da8c77743c874b6e91a3d59e73f8265ee9e24d95
andresdiaz10 <andresdiaz5010@icloud.com>
2021-06-25 02:49:18 +0000

    Fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,15 @@ void more_numbers(void)
 	int i;
 	int number;
 
-	for (i = 0 ; i < 11 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		for (number = 0 ; number < 15 ; number++)
 		{
 			if (number > 9)
 			{
-				_putchar((i / 10) + '0');
+				_putchar((number / 10) + '0');
 			}
-			_putchar((i % 10) + '0');
+			_putchar((number % 10) + '0');
 		}
 		_putchar('\n');
 	}
