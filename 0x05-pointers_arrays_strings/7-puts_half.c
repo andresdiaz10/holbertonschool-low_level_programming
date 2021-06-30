64374e5812327ec293d8bcf467fe6c30e6292707
andresdiaz10 <andresdiaz5010@icloud.com>
2021-06-30 04:43:41 +0000

    Task - 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * puts_half - Prints half
+ * @str: String
+ */
+void puts_half(char *str)
+{
+	int size = 0;
+	int half_size = 0;
+	int i = 0;
+
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	size++;
+	half_size = (size - 1) / 2;
+	for (i = half_size ; i <= size ; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
