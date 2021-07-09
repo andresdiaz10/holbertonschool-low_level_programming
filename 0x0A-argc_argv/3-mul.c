cd8e903b77b3d8569721df07df76088b6be14222
Andres Diaz <andresdiaz5010@icloud.com>
2021-07-09 22:35:18 +0000

    Task -3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Print the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array of the command line arguments
+ *
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	if (argc == 1 || argc >= 4)
+	{
+	    printf("%s\n", "Error");
+	}
+	else
+	{
+	    printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
+}
