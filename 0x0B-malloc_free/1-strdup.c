2af6e59754cc72637d8dbe9e9edf92ceb62ca6e8
Andres Diaz <andresdiaz5010@icloud.com>
2021-07-15 00:15:40 +0000

    Task -1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,11 +14,11 @@ char *_strdup(char *str)
 	size_t size_str = 0;
 	size_t i = 0;
 
+	if (!(str))
+		return ('\0');
 	for (i = 0 ; *(str + i)  != '\0' ; i++)
 		size_str++;
 	arr = (char *)malloc(size_str * sizeof(char));
-	if (!(str))
-		return ('\0');
 	if (arr)
 	{
 		for (i = 0 ; *(str + i) != '\0' ; i++)
