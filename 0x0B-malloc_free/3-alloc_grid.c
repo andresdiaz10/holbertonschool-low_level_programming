9a51aa0992d5a823d135b9c79a4e6f1ab3c366c8
Andres Diaz <andresdiaz5010@icloud.com>
2021-07-19 00:54:32 +0000

    Task 3 fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,6 +17,19 @@ void fill_array(int **arr, int width, int height)
 			arr[i][j] = 0;
 }
 /**
+ * free_array - Freed when malloc fails
+ * @arr: array
+ * @height: Height of the array
+ */
+void free_array(int **arr, int height)
+{
+	int i;
+
+	for (i = 0 ; i < height ; i++)
+		free(arr[i]);
+	free(arr);
+}
+/**
  * alloc_grid - grid a to a 2 dimensional array of integers
  * @height: height of the matrix
  * @width: width of the matrix
@@ -32,12 +45,18 @@ int **alloc_grid(int width, int height)
 		return ('\0');
 	arr = (int **)malloc(height * sizeof(int *));
 	if (!(arr))
+	{
+		free_array(arr, height);
 		return ('\0');
+	}
 	for (i = 0 ; i < height ; i++)
 	{
 		arr[i] = (int *)malloc(width * sizeof(int));
 		if (!(arr[i]))
+		{
+			free_array(arr, height);
 			return ('\0');
+		}
 	}
 	fill_array(arr, width, height);
 	return (arr);
