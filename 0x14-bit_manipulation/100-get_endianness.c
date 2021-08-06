ce483fcb27c81264f31692b59315ac8380b28d9c
Andres Diaz <andresdiaz5010@icloud.com>
2021-08-06 02:55:33 +0000

    Task -6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: if little endian 1
+ *	if big endian 0
+ */
+
+int get_endianness(void)
+{
+	int aux = 1;
+	char *endian = (void *)&aux;
+
+	if (*endian == 1)
+		return (1);
+	return (0);
+}
