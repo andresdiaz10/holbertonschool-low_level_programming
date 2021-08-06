87822b1628881542990b7a1ba0b49f3c863d8037
Andres Diaz <andresdiaz5010@icloud.com>
2021-08-06 16:50:51 +0000

    Task - 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - number of bits needed to be flipped to get
+ *	from one number to anoter
+ *
+ * @n: number
+ * @m: number to flip
+ *
+ * Return: bit nessary to  flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = n ^ m, bit = 0;
+
+	while (xor > 0)
+	{
+		bit += (xor & 1);
+		xor >>= 1;
+	}
+
+	return (bit);
+}
