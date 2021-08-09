e85e16858fdd2bc4298b0e987f20e2de423f5474
andresdiaz10 <70444970+andresdiaz10@users.noreply.github.com>
2021-08-09 13:58:53 -0500

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,15 +11,15 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int text_size = 0;
-	int open_b;
-	int write_b;
+	size_t text_size;
+	ssize_t open_b;
+	ssize_t write_b;
 
 	if (!(filename))
 		return (-1);
 	if (text_content)
 	{
-		while(*(text_content + text_size))
+		for (text_size = 0; text_content[text_size])
 			text_size++;
 	}
 	open_b = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
