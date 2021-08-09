572e18a5bebdd9dc743cec9e2c5f26c697f96d7d
Andres Diaz <andresdiaz5010@icloud.com>
2021-08-09 19:01:16 +0000

    Fix Task -1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		for (text_size = 0; text_content[text_size])
+		for (text_size = 0 ; text_content[text_size] ;)
 			text_size++;
 	}
 	open_b = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
