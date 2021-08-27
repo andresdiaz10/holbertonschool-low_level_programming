ae53a2d8032380827a66644fc9b37f2a1037ecc6
andresdiaz10 <70444970+andresdiaz10@users.noreply.github.com>
2021-08-27 11:33:18 -0500

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,7 +9,7 @@
  * Return: a
  */
 
-char *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 	unsigned char *destination = dest;
