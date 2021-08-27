26b2780a1c1f366dcca5a3a98ba593256c06a5e0
Andres Diaz <andresdiaz5010@icloud.com>
2021-08-27 16:41:37 +0000

    Fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,7 +9,7 @@
  * Return: a
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int index;
 	unsigned char *destination = dest;
